b283206748b1fe4d78ea1322d08158bd81d93e1e
GuestSneezeOSDev <163439609+GuestSneezeOSDev@users.noreply.github.com>
2024-12-29 13:36:17 +0400

    Improve the build system!
    Co-Authored-By: Luke Short <ekultails@gmail.com>

---
+++ env/gsos_default.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Yep, this script is inspired by winesapOS. Check it out! 
+# https://github.com/winesapOS/winesapOS
+export \
+  GUESTSNEEZEOSOS_DE="${GUESTSNEEZEOS_DE:-plasma}" \
+  GUESTSNEEZEOS_GAMING_TOOLS="${GUESTSNEEZEOS_GAMING_TOOLS:-true}" \
+  GUESTSNEEZEOS_BOOTLOADER="${GUESTSNEEZEOS_BOOTLOADER:-grub}" \
---
+++ guestsneezeo_install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Yep, this script is inspired by winesapOS. Check it out! 
+# https://github.com/winesapOS/winesapOS
+
+# Load default environment variables.
+../env/gsos_default.sh
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root. Please use sudo." 1>&2
+   exit 1
+fi
+
+if [[ "${GUESTSNEEZEOS_DE}" == "plasma" ]]; then
+   echo "Installing the KDE Plasma desktop environment..."
+   echo "plasma-meta" >> "src/packages.x86_64"
+   echo "plasma-nm" >> "src/packages.x86_64"
+   # The KDE Plasma theme is by @LukeShortCloud, go follow him!
+   cd ../ # Move to parent directory
+   wget https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-rel/os/x86_64/steamdeck-kde-presets-0.16-1-any.pkg.tar.zst
+   zstd -d steamdeck-kde-presets-0.16-1-any.pkg.tar.zst
+   mv steamdeck-kde-presets-0.16-1-any.pkg.tar/ GuestSneezeOS/
+   cd GuestSneezeOS/
+   cd steamdeck-kde-presets-0.16-1-any.pkg.tar/
+   mv etc/ ../airootfs/
+   mv usr/ ../airootfs/
+   cd ../
+   rm -rf steamdeck-kde-presets-0.16-1-any.pkg.tar/
+   
+elif [[ "${GUESTSNEEZEOS_DE}" == "hyprland" ]]; then
+   echo "Hyprland is coming soon!"
+fi
+
+
+# Finally, build
+sudo rm -rf out/ work/
+sudo src/archiso/archiso/mkarchiso -v -w work/ -o out/ src/ 
