f9889cb4a16f41fad38c2e3a123f65eeb3a5189d
GuestSneezeOSDev <163439609+GuestSneezeOSDev@users.noreply.github.com>
2024-12-29 12:50:59 +0400

    Re-order

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-sudo rm -rf ../out
-sudo archiso/archiso/mkarchiso -v -w ../out -o ../out ../GuestSneezeOS 
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root. Please use sudo." 1>&2
+   exit 1
+fi
+sudo rm -rf out/ work/
+sudo src/archiso/archiso/mkarchiso -v -w work/ -o out/ src/ 
 
